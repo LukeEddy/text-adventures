f9aaf6ed7ea05d4246fffc0eaa74f71634d12a86
Merit Student <student@meac.local>
2018-09-12 13:36:47 -0600

    text adventure

---
+++ text.py
@@ -0,0 +1,10 @@
+print ("to start type start")
+fromUser = input()
+while True:
+    if fromUser == "quit": # else if 
+        print ("Game over")
+        break # end the white loop
+        else:
+            else:
+                print("tp start type start")
+                fromUser = input()
